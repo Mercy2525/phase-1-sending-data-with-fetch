d93ec6193425d0d30bf15269a2580c8955e9a706
ASUS <you@example.com>
2023-07-26 16:54:17 +0300

    Post

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+
+function submitData(name, email){
+    return fetch("http://localhost:3000/users", {
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json",
+        "Accept": "application/json"
+    },
+    body:JSON.stringify({
+        name:name,
+        email:email
+    })
+})
+.then(resp=>resp.json())
+.then(data=> {
+    let newId=data.id
+    let idElement=document.createElement("p")
+    idElement.textContent=`New Id: ${newId}`
+    document.body.appendChild(idElement)
+})
+.catch(error=> {
+    let testError=document.createElement("p")
+    testError.textContent=`ERROR: ${error.message}`
+    document.body.appendChild(testError)
+})   
+
+}
+
+let form=document.querySelector("#input")
+  form.addEventListener("submit",function(e){
+    e.preventDefault()
+    let name= document.getElementById('nameId').value
+    let email= document.getElementById('emailId').value;
+    submitData(name,email)
+  })
+
+
